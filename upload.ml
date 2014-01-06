88716534cc9a9c19b4241be7ea2b0c95100c1c0e
Erkki Seppälä <flux@modeemi.fi>
2014-01-06 16:10:17 +0200

    Upload: perform serial port locking

--- upload.ml
+++ upload.ml
@@ -1,7 +1,39 @@
 open Batteries
 open Common
 
-let open_serial device bps =
+let strip = Pcre.replace ~pat:"[\r\n]*$"
+
+let lockfile_of_device_name device_name =
+  let device_base = (Pcre.extract ~pat:"([^/]*)$" ~full_match:false device_name).(0) in
+  let lockfile = Printf.sprintf "/var/lock/LCK..%s" device_base in
+  lockfile
+
+let lock_serial device_name =
+  let lockfile = lockfile_of_device_name device_name in
+  let locking_pid = 
+    try Some (int_of_string (strip (input_file lockfile)))
+    with Sys_error _ -> None
+  in
+  let locked =
+    match locking_pid with
+    | Some pid ->
+      ( try Unix.kill pid 0; true
+	with Unix.Unix_error (Unix.ESRCH, _, _) -> false )
+    | None -> false
+  in
+  (* should also check if the lock file is older than the process in question? *)
+  if locked then (
+    Printf.eprintf "%s is locked by pid %d\n%!" device_name (Option.get locking_pid);
+    false
+  ) else (
+    File.with_file_out ~mode:[`create; `trunc] (* ~perm:(File.unix_perm 0644) *) lockfile @@ fun f ->
+      Printf.fprintf f "%d\n%!" (Unix.getpid ());
+      true
+  )
+
+let unlock_serial device_name = Unix.unlink (lockfile_of_device_name device_name)
+
+let open_serial (device, bps) =
   let fd = Unix.openfile device [Unix.O_RDWR] 0 in
   let tio = Unix.tcgetattr fd in
   let tio = { 
@@ -22,6 +54,20 @@ let open_serial device bps =
   } in
   let _ = Unix.tcsetattr fd Unix.TCSANOW tio in
   fd
+
+let with_serial ((device_name, bps) as open_args) f =
+  if lock_serial device_name then
+    let doit () =
+      let fd = open_serial open_args in
+      let v = wrap f fd in
+      Unix.close fd;
+      ok v
+    in
+    let v = wrap doit () in
+    unlock_serial device_name;
+    ok v
+  else
+    failwith "Serial port is locked"
 
 let get_assoc json =
   match json with
@@ -150,35 +196,35 @@ let get_gcode lines =
   lines |> Enum.map (strip_comments %> strip_whitespace) |> Enum.filter ((<>) "") |> List.of_enum
 
 let upload common_options file =
-  let fd = open_serial common_options.co_device common_options.co_bps in
-  let input_gcode = get_gcode (File.lines_of file)  in
-  let signal_fds = Unix.pipe () in
-  let task_queue = Protect.create (Mutex.create ()) (Queue.create ()) in
-  let thread = Thread.create receiver (fd, fst signal_fds, task_queue) in
-  let t = { fd; signal_fd = Protect.create (Mutex.create ()) (ref (Some (snd signal_fds))); lines_sent = ref 0; task_queue } in
-  let ready = new Future.t in
-  let rec feed_lines input =
-    match input with
-    | [] -> 
-      Printf.printf "Done!\n%!";
-      ready#set ();
-    | command::rest ->
-      (send_gcode t command)#add_callback @@ function
-      | `Ok r -> 
-	feed_lines rest
-      | `Aborted -> 
-	Printf.printf "Meh, error sending request!\n%!";
-	ready#set ()
-  in
-  feed_lines input_gcode;
-  ready#wait ();
-  Unix.sleep 5;
-  Protect.access t.signal_fd (
-    function
-    | { contents = None } -> assert false;
-    | { contents = Some fd } as sfd ->
-      Unix.close fd;
-      sfd := None
-  );
-  Thread.join thread;
-  ()
+  with_serial (common_options.co_device, common_options.co_bps) @@ fun fd ->
+    let input_gcode = get_gcode (File.lines_of file)  in
+    let signal_fds = Unix.pipe () in
+    let task_queue = Protect.create (Mutex.create ()) (Queue.create ()) in
+    let thread = Thread.create receiver (fd, fst signal_fds, task_queue) in
+    let t = { fd; signal_fd = Protect.create (Mutex.create ()) (ref (Some (snd signal_fds))); lines_sent = ref 0; task_queue } in
+    let ready = new Future.t in
+    let rec feed_lines input =
+      match input with
+      | [] -> 
+	Printf.printf "Done!\n%!";
+	ready#set ();
+      | command::rest ->
+	(send_gcode t command)#add_callback @@ function
+	| `Ok r -> 
+	  feed_lines rest
+	| `Aborted -> 
+	  Printf.printf "Meh, error sending request!\n%!";
+	  ready#set ()
+    in
+    feed_lines input_gcode;
+    ready#wait ();
+    Unix.sleep 5;
+    Protect.access t.signal_fd (
+      function
+      | { contents = None } -> assert false;
+      | { contents = Some fd } as sfd ->
+	Unix.close fd;
+	sfd := None
+    );
+    Thread.join thread;
+    ()
