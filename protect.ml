a0050911d9ca98259d78a8cc7b5fed0c771a3b3a
Erkki Seppälä <flux@modeemi.fi>
2014-01-06 03:15:19 +0200

    Protect.create: renamed from make

--- protect.ml
+++ protect.ml
@@ -1,7 +1,7 @@
 open Batteries
 
 type 'a t = ('a * Mutex.t)
-let make mutex value = (value, mutex)
+let create mutex value = (value, mutex)
 let access (value, mutex) f = 
   Mutex.lock mutex;
   let v = wrap f value in
--- protect.mli
+++ protect.mli
@@ -1,4 +1,4 @@
 type 'a t
-val make : Mutex.t -> 'a -> 'a t
+val create : Mutex.t -> 'a -> 'a t
 val access : 'a t -> ('a -> 'b) -> 'b
 
