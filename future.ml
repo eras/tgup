54e0c4f5e3f4fe906d5ab6d13aaa2dc8ffa63a19
Erkki Seppälä <flux@modeemi.fi>
2014-01-06 03:15:59 +0200

    Future: added a module for representing futures

---
+++ future.ml
@@ -0,0 +1,14 @@
+class ['a] t =
+object
+  val mutable value : 'a option = None
+  val mutable callbacks : ('a -> unit) list = []
+  method get = value
+  method set (x : 'a) =
+    assert (value = None);
+    value <- Some x;
+    List.iter (fun cb -> cb x) callbacks
+  method add_callback (cb : 'a -> unit) =
+    match value with
+    | None -> callbacks <- cb::callbacks
+    | Some x -> cb x
+end
