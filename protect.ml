2c5dfbfb0a163e2dfbebb8bf76b90477cda6648a
Erkki Seppälä <flux@modeemi.fi>
2014-01-06 13:47:01 +0200

    Protect: added ability to wait on a builtin or a given condition

--- protect.ml
+++ protect.ml
@@ -1,9 +1,19 @@
 open Batteries
 
-type 'a t = ('a * Mutex.t)
-let create mutex value = (value, mutex)
-let access (value, mutex) f = 
+type 'a t = ('a * Mutex.t * Condition.t)
+let create ?cond mutex value = (value, mutex, Option.default (Condition.create ()) cond)
+let access (value, mutex, condition) f = 
   Mutex.lock mutex;
   let v = wrap f value in
+  Condition.broadcast condition;
   Mutex.unlock mutex;
   ok v
+let wait_access (value, mutex, condition) p f =
+  access (value, mutex, condition) @@ fun value ->
+    let rec wait () =
+      match wrap p value with
+      | Ok true -> ()
+      | Ok false -> wait ()
+      | Bad exn -> raise exn
+    in
+    f value
--- protect.mli
+++ protect.mli
@@ -1,4 +1,6 @@
 type 'a t
-val create : Mutex.t -> 'a -> 'a t
+val create : ?cond:Condition.t -> Mutex.t -> 'a -> 'a t
 val access : 'a t -> ('a -> 'b) -> 'b
 
+(* [wait t condition f] waits condition to be true and then acts like 'access' *)
+val wait_access : 'a t -> ('a -> bool) -> ('a -> 'b) ->'b
