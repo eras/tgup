df4a9a25e9a00f2a62783d0b7843d888a8b5c9e4
Erkki Seppälä <flux@modeemi.fi>
2014-01-06 17:09:09 +0200

    Upload: Beginnings of proper flow control

--- upload.ml
+++ upload.ml
@@ -1,6 +1,8 @@
 open Batteries
 open Common
 
+module Json = Yojson.Safe
+
 let get_assoc json =
   match json with
   | `Assoc x -> x
@@ -26,22 +28,54 @@ let get_linenumber (`Result result) =
   with Not_found -> None
 
 let get_tinyg str =
-  let a = get_assoc (Yojson.Safe.from_string str) in
+  let a = get_assoc (Json.from_string str) in
   try `Result (get_assoc (List.assoc "r" a))
-  with Not_found -> `Status a
+  with Not_found -> 
+    try `Status (get_assoc (List.assoc "sr" a))
+    with Not_found -> `Other a
 
-type result = (string * Yojson.Safe.json) list
+type result = (string * Json.json) list
 
 type linenumber = int
 
+type gcode_request = (string * (result -> unit))
+
 type rt = {
   rt_line_callbacks : (linenumber, result -> unit) Hashtbl.t;
+  rt_send_gcode : gcode_request -> unit; (* when you want a G-code request enqueued, call this. *)
 }
 
+let add_line_callback rt n fn =
+  match n with
+  | None -> failwith "fill in code"
+  | Some n -> Hashtbl.add rt.rt_line_callbacks n fn
+
+let activate future x = 
+  future#set (`Ok x)
+
 let receiver (fd, signal_fd, task_queue) =
-  let rt = { rt_line_callbacks = Hashtbl.create 1024 } in
+  let slots_available = ref 1 in
+  let command_queue : gcode_request Queue.t = Queue.create () in
+  let lines_sent = ref 0 in
+  let rt = {
+    rt_line_callbacks = Hashtbl.create 1024;
+    rt_send_gcode = fun x -> Queue.add x command_queue;
+  } in
   let buf = String.create 1024 in
   let lb = LineBuffer.create () in
+  let last_linenumber = ref 0 in
+  let rec flush_queue () =
+    if !slots_available > 0 && not (Queue.is_empty command_queue) then
+      let gcode, callback = Queue.take command_queue in
+      incr lines_sent;
+      let linenumber = !lines_sent in
+      let msg = Printf.sprintf "{\"gc\":\"%s N%d\"}\r\n" gcode linenumber in
+      Printf.printf "-> %s%!" msg;
+      add_line_callback rt (Some linenumber) callback;
+      let _n = Unix.write fd msg 0 (String.length msg) in
+      (* decr slots_available; -- disabled for now *)
+      flush_queue ()
+  in
   let rec feed_lines () =
     let (rd, _, _) = Unix.select [fd; signal_fd] [] [] (-1.0) in
     match () with
@@ -64,6 +98,18 @@ let receiver (fd, signal_fd, task_queue) =
 	      Printf.printf "Response without linenumber\n%!"
 	    )
 	  | `Status status ->
+	    let current_linenumber = 
+	      try Some (get_int (List.assoc "line" status)) 
+	      with Not_found -> None in
+	    ( match current_linenumber with
+	    | None -> ()
+	    | Some current_linenumber ->
+	      let delta = current_linenumber - !last_linenumber in
+	      slots_available := !slots_available + delta;
+	      Printf.printf "%d new slots, %d in total\n%!" delta !slots_available;
+	      flush_queue ()
+	    )
+	  | `Other _ ->
 	    ()
 	)
 	strings;
@@ -75,21 +121,16 @@ let receiver (fd, signal_fd, task_queue) =
       else 
 	let task = Protect.access task_queue Queue.take in
 	task rt;
+	flush_queue ();
 	feed_lines ()
     | _ -> ()
   in
   feed_lines ();
   Printf.printf "Receiver finished\n%!"
 
-let add_line_callback rt n fn =
-  match n with
-  | None -> failwith "fill in code"
-  | Some n -> Hashtbl.add rt.rt_line_callbacks n fn
-
 type t = {
   fd	     : Unix.file_descr;
   signal_fd  : Unix.file_descr option ref Protect.t;
-  lines_sent : int ref;
   task_queue : (rt -> unit) Queue.t Protect.t;
 }
 
@@ -101,22 +142,11 @@ let request t fn =
     Protect.access t.task_queue (Queue.add (fun rt -> fn (`Ok rt)));
     ignore (Unix.write fd "1" 0 1)
 
-let activate future x = 
-  future#set (`Ok x)
-
 let send_gcode t gcode =
   let activation = new Future.t in
   request t (function
-  | `Ok rt ->
-    incr t.lines_sent;
-    let linenumber = !(t.lines_sent) in
-    let msg = Printf.sprintf "{\"gc\":\"%s N%d\"}\r\n" gcode linenumber in
-    Printf.printf "-> %s%!" msg;
-    add_line_callback rt (Some linenumber) (activate activation);
-    let _n = Unix.write t.fd msg 0 (String.length msg) in
-    ()
-  | `Aborted ->
-    activation#set `Aborted
+  | `Ok rt -> rt.rt_send_gcode (gcode, activate activation)
+  | `Aborted -> activation#set `Aborted
   );
   activation
 
@@ -128,12 +158,12 @@ let get_gcode lines =
   lines |> Enum.map (strip_comments %> strip_whitespace) |> Enum.filter ((<>) "") |> List.of_enum
 
 let upload common_options file =
-  with_serial (common_options.co_device, common_options.co_bps) @@ fun fd ->
+  Serial.with_serial (common_options.co_device, common_options.co_bps) @@ fun fd ->
     let input_gcode = get_gcode (File.lines_of file)  in
     let signal_fds = Unix.pipe () in
     let task_queue = Protect.create (Mutex.create ()) (Queue.create ()) in
     let thread = Thread.create receiver (fd, fst signal_fds, task_queue) in
-    let t = { fd; signal_fd = Protect.create (Mutex.create ()) (ref (Some (snd signal_fds))); lines_sent = ref 0; task_queue } in
+    let t = { fd; signal_fd = Protect.create (Mutex.create ()) (ref (Some (snd signal_fds))); task_queue } in
     let ready = new Future.t in
     let rec feed_lines input =
       match input with
