a06cf9cb70c83f58815b0341e5d47d05e640216f
Erkki Seppälä <flux@modeemi.fi>
2014-02-04 10:02:30 +0200

    Future: added dependencies so that the weak pointers don't collected too soon

--- future.ml
+++ future.ml
@@ -2,11 +2,15 @@ open Batteries
 
 module Weaktbl = BatInnerWeaktbl
 
+type never
+type dependency = unit -> never (* used to keep a reference around, the function isn't actually called *)
+
 class ['a] t =
 object
   val mutex = Protect.create (Mutex.create ()) ()
   val mutable value : 'a option = None
   val mutable callbacks : (('a -> unit), unit) Weaktbl.t = Weaktbl.create 1
+  val mutable dependencies : dependency list = []
   method get = Protect.access mutex @@ fun () -> value
   method wait () =
     Protect.wait_access mutex (fun () -> value <> None) @@ fun () ->
@@ -17,6 +21,7 @@ object
     let cbs =
       Protect.access mutex @@ fun () ->
 	assert (value = None);
+	dependencies <- [];
 	value <- Some x;
 	callbacks
     in
@@ -25,6 +30,7 @@ object
     let was_set, cbs =
       Protect.access mutex @@ fun () ->
 	if value = None then (
+	  dependencies <- [];
 	  value <- Some x;
 	  (true, callbacks)
 	) else (
@@ -38,20 +44,28 @@ object
       match value with
       | None -> Weaktbl.add callbacks cb (); ignore
       | Some x -> (fun () -> cb x)) ()
+  method add_dependency dep =
+    Protect.access mutex @@ fun () ->
+      if value = None then
+	dependencies <- dep::dependencies;
 end
 
 type ('a, 'b) future_cb = (< add_callback : ('a -> unit) -> unit; .. > as 'b)
 
 let map f t =
   let future = new t in
-  t#add_callback (fun x -> future#set (f x));
+  let callback = fun x -> future#set (f x) in
+  future#add_dependency (fun () -> callback (assert false); assert false);
+  t#add_callback callback;
   future
 
 let wait ts =
   let future = new t in
+  let callback = (fun x -> future#set x) in
+  future#add_dependency (fun () -> callback (assert false); assert false);
   List.iter 
     (fun t ->
-      t#add_callback (fun x -> future#set x);
+      t#add_callback callback;
     )
     ts;
   future#wait ()
--- future.mli
+++ future.mli
@@ -1,3 +1,6 @@
+type never
+type dependency = unit -> never (* used to keep a reference around, the function isn't actually called *)
+
 class ['a] t :
   object
     method add_callback : ('a -> unit) -> unit
@@ -5,6 +8,7 @@ class ['a] t :
     method set		: 'a -> unit
     method set_if_unset	: 'a -> bool	(* returns true if value was set *)
     method wait		: unit -> 'a
+    method add_dependency : dependency -> unit
   end
 
 type ('a, 'b) future_cb = (< add_callback : ('a -> unit) -> unit; .. > as 'b)
