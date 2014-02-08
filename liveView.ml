d02580965ac8b7d275ed1165f4089f9f3e04b837
Erkki Seppälä <flux@modeemi.fi>
2014-02-09 01:25:50 +0200

    LiveView: don't convert only stride, but also rgb to rgbx

--- liveView.ml
+++ liveView.ml
@@ -11,20 +11,21 @@ let show_exn f =
     Printf.printf "Exception: %s (%s)\n%!" (Printexc.to_string exn) (Printexc.get_backtrace ());
     raise exn
 
-let convert_stride stride (width, height) rgb_data =
-  if stride = width * 3
-  then rgb_data
-  else
-    let open Bigarray in
-    let open Array1 in
-    let rgb_data_out = create int8_unsigned c_layout (stride * height) in
-    for y = 0 to height - 1 do
-      blit
-	(sub rgb_data (y * 3 * width) (3 * width))
-	(sub rgb_data_out (y * stride) (3 * width))
-    done;
-    rgb_data_out
-
+let convert_rgb_to_rgbx (width, height) rgb_data =
+  let open Bigarray in
+  let open Array1 in
+  let stride = Cairo.Image.stride_for_width RGB24 width in
+  let rgbx_data = create int8_unsigned c_layout (stride * height) in
+  Printf.printf "width: %d\n%!" width;
+  for y = 0 to height - 1 do
+    for x = 0 to width - 1 do
+      for c = 0 to 2 do
+	rgbx_data.{y * stride + (x * 4) + c} <-
+	  rgb_data.{y * width * 3 + (x * 3) + c}
+      done
+    done
+  done;
+  (stride, rgbx_data)
 
 let view (width, height) ?packing () =
   let drawing_area = GMisc.drawing_area ?packing ~width ~height () in
@@ -69,10 +70,9 @@ let view (width, height) ?packing () =
   let interface = object
     method set_image ((width, height), rgb_data) =
       let format = Cairo.Image.RGB24 in
-      let stride = Cairo.Image.stride_for_width format width in
-      let rgb_data = convert_stride stride (width, height) rgb_data in
+      let (stride, rgb_data) = convert_rgb_to_rgbx (width, height) rgb_data in
       Printf.printf "%d,%d %d\n%!" width height (Bigarray.Array1.dim rgb_data);
-      image := Some (Cairo.Image.create_for_data8  ~stride rgb_data format width height, width, height);
+      image := Some (Cairo.Image.create_for_data8 ~stride rgb_data format width height, width, height);
       drawing_area#misc#draw None
   end in
   (* let count = ref 0 in  *)
