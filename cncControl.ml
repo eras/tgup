6c65b0ea40c30e1f1f8ec6d2400a34a8d1a31c3e
Erkki Seppälä <flux@modeemi.fi>
2014-02-09 15:31:15 +0200

    CncControl: added ability to set travel length

--- cncControl.ml
+++ cncControl.ml
@@ -1,17 +1,21 @@
 open Gtk
 
 let view ~packing cnc () =
-  let layout = GPack.table ~packing ~columns:3 ~rows:3 () in
+  let vbox = GPack.vbox ~packing () in
+  let directionals = GPack.table ~packing:(vbox#add) ~columns:3 ~rows:3 () in
   let _ = Cnc.wait cnc (Cnc.set_feed_rate 100.0) in
   let _ = Cnc.wait cnc Cnc.set_relative in
+  let (travel_length, _) = GEdit.combo_box_entry_text ~strings:["0.01"; "0.1"; "1.0"; "10.0"; "50.0"] ~packing:vbox#pack () in
+  travel_length#entry#set_text "1.0";
   let move x_dir y_dir _ =
-    let x_ofs = float x_dir *. 1.0 in
-    let y_ofs = float y_dir *. 1.0 in
+    let length = float_of_string (travel_length#entry#text) in
+    let x_ofs = float x_dir *. length in
+    let y_ofs = float y_dir *. length in
     Cnc.ignore cnc (Cnc.travel [`X x_ofs; `Y y_ofs])
   in
-  (GButton.button ~label:"Y+" ~packing:(layout#attach ~left:1 ~top:0) ())#connect#clicked (move (0) (1));
-  (GButton.button ~label:"Y-" ~packing:(layout#attach ~left:1 ~top:2) ())#connect#clicked (move (0) (-1));
-  (GButton.button ~label:"X+" ~packing:(layout#attach ~left:2 ~top:1) ())#connect#clicked (move (1) (0));
-  (GButton.button ~label:"X-" ~packing:(layout#attach ~left:0 ~top:1) ())#connect#clicked (move (-1) (0));
+  ignore ((GButton.button ~label:"Y+" ~packing:(directionals#attach ~left:1 ~top:0) ())#connect#clicked (move (0) (1)));
+  ignore ((GButton.button ~label:"Y-" ~packing:(directionals#attach ~left:1 ~top:2) ())#connect#clicked (move (0) (-1)));
+  ignore ((GButton.button ~label:"X+" ~packing:(directionals#attach ~left:2 ~top:1) ())#connect#clicked (move (1) (0)));
+  ignore ((GButton.button ~label:"X-" ~packing:(directionals#attach ~left:0 ~top:1) ())#connect#clicked (move (-1) (0)));
   ()
 
