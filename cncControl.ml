70adc0537f03b304d9e3cd2f9744196188314a07
Erkki Seppälä <flux@modeemi.fi>
2014-02-09 23:10:12 +0200

    CncControl: added a primitive way for retreiving current location

--- cncControl.ml
+++ cncControl.ml
@@ -7,15 +7,20 @@ let view ~packing cnc () =
   let _ = Cnc.wait cnc Cnc.set_relative in
   let (travel_length, _) = GEdit.combo_box_entry_text ~strings:["0.01"; "0.1"; "1.0"; "10.0"; "50.0"] ~packing:vbox#pack () in
   travel_length#entry#set_text "1.0";
+  let cnc_x = ref 0.0  in
+  let cnc_y = ref 0.0 in
   let move x_dir y_dir _ =
     let length = float_of_string (travel_length#entry#text) in
     let x_ofs = float x_dir *. length in
     let y_ofs = float y_dir *. length in
+    cnc_x := !cnc_x +. x_ofs;
+    cnc_y := !cnc_y +. y_ofs;
     Cnc.ignore cnc (Cnc.travel [`X x_ofs; `Y y_ofs])
   in
   ignore ((GButton.button ~label:"Y+" ~packing:(directionals#attach ~left:1 ~top:0) ())#connect#clicked (move (0) (1)));
   ignore ((GButton.button ~label:"Y-" ~packing:(directionals#attach ~left:1 ~top:2) ())#connect#clicked (move (0) (-1)));
   ignore ((GButton.button ~label:"X+" ~packing:(directionals#attach ~left:2 ~top:1) ())#connect#clicked (move (1) (0)));
   ignore ((GButton.button ~label:"X-" ~packing:(directionals#attach ~left:0 ~top:1) ())#connect#clicked (move (-1) (0)));
-  ()
-
+  object 
+    method get_position = (!cnc_x, !cnc_y)
+  end
