92657cf804c3dec8a433ebc6b7d60ef328227d5c
Erkki Seppälä <flux@modeemi.fi>
2014-02-27 22:38:19 +0200

    CncControl: viewport position is now to to the other way from coord mode offset.. maybe wrong, but at least consistent.

--- src/cncControl.ml
+++ src/cncControl.ml
@@ -108,7 +108,7 @@ let view ~packing cnc () =
   in
   object 
     method get_position = Gg.V3.v !cnc_x !cnc_y (!cnc_z -. !reference_z)
-    method get_viewport_position = Gg.V2.sub (Gg.V2.v !cnc_x !cnc_y) (coord_mode_offset !coord_mode)
+    method get_viewport_position = Gg.V2.add (Gg.V2.v !cnc_x !cnc_y) (coord_mode_offset !coord_mode)
     method adjust_position xy = move_by (Gg.V2.x xy) (Gg.V2.y xy)
     method position_adjust_callback = position_adjust_callback
 
