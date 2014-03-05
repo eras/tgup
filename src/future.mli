ead0e3be27d721b79eae154402e821f4f1862abc
Erkki Seppälä <flux@modeemi.fi>
2014-03-05 20:19:55 +0200

    Future: mapped futures are not settable

--- src/future.mli
+++ src/future.mli
@@ -15,7 +15,7 @@ type dependency = unit -> never (* used to keep a reference around, the function
 
 exception FutureAlreadySet
 
-class ['a] t :
+class type ['a] readonly =
 object
   (* [add_callback cb] adds function cb to be called when a value is
      assigned (it may be called immediately if the value is already
@@ -30,15 +30,6 @@ object
   (* [get] gets the current value of the future. It may be None or Some x. *)
   method get		: 'a option
 
-  (* [set x] sets a value to the future. Once a value has been set it
-     cannot be unset or reset to another value. Attempting to do that
-     will raise [FutureAlreadySet]. *)
-  method set		: 'a -> unit
-
-  (* [set_if_unset x] is similar to set, except it will simply return true
-     if the value was already set, otherwise it will return false. *)
-  method set_if_unset	: 'a -> bool
-
   (* [wait] synchronously waits for a value to be set to the future and
      returns with the value once it has been set. It may also return
      immediately if the future already has a value. *)
@@ -51,12 +42,25 @@ object
   method add_dependency : dependency -> unit
 end
 
+class ['a] t :
+object
+  inherit ['a] readonly
+  (* [set x] sets a value to the future. Once a value has been set it
+     cannot be unset or reset to another value. Attempting to do that
+     will raise [FutureAlreadySet]. *)
+  method set		: 'a -> unit
+
+  (* [set_if_unset x] is similar to set, except it will simply return true
+     if the value was already set, otherwise it will return false. *)
+  method set_if_unset	: 'a -> bool
+end
+
 (* The callback-aspect of the future class *)
 type ('a, 'b) future_cb = (< add_callback : ('a -> unit) -> dependency; add_persistent_callback : ('a -> unit) -> unit; .. > as 'b)
 
 (* [map f future] creates a new future that maps the value of a given function with the
    given functino. Throwing an exception here may pop up at unexpected places.. *)
-val map : ('a -> 'b) -> ('a, _) future_cb -> 'b t
+val map : ('a -> 'b) -> ('a, _) future_cb -> 'b readonly
 
 (* [wait futures] waits the first future to be active and returns that value. Note: It can be
    non-deterministic which value is set if two futures are active simulatenously or upon entrance. *)
